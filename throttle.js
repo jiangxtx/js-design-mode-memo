99cdfbd9c39c5075fd1876f4f459b88ab8faf750
XiaJiujiang <xiajiujiang@foxmail.com>
2017-12-08 16:04:45 +0800

    throttle src

---
+++ throttle.js
@@ -0,0 +1,16 @@
+/**
+ * @description
+ * @author xiajiujiang239
+ * @createtime 2017/12/8
+ */
+
+function throttle(func, wait, options) {
+  var context, args, result;
+  var timeout = null;
+  var previous = 0;
+  if (!options) options = {};
+  
+  var later = function () {
+    
+  }
+}
